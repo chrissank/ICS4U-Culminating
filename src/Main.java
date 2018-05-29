e6937285af6991c0bae56ab65534fbc57b8db5b3
Pixelatorx2 <csankey6@gmail.com>
2018-05-29 12:59:10 -0400

    Stream msg

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	// Test
-
-    public static void main(String[] args) {
-        
+    //Main.class.getResourceAsStream("/resources/strings.txt"))
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/resources/strings.txt")));
+        System.out.println(br.readLine());
     }
     
 }
