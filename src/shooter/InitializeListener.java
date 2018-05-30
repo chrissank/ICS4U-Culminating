45d26333acef295d91c6fd952cabafc907b5880a
Chris <Chris@172.19.95.9>
2018-05-30 13:31:32 -0400

    Start switch to listeners and events also music

---
+++ src/shooter/InitializeListener.java
@@ -0,0 +1,23 @@
+package shooter;
+
+import shooter.events.EventListener;
+import shooter.events.Listener;
+import shooter.events.types.GameInitiateEvent;
+import shooter.game.GameDisplay;
+import shooter.menu.shooterMenu;
+
+public class InitializeListener implements Listener {
+
+    Main main;
+    
+    @EventListener
+    public void onInit(GameInitiateEvent e) {
+        main = Main.getInstance();
+        //playClip();
+        main.frame = new GameFrame();
+        main.menu = new shooterMenu();
+        main.frame.add(main.menu, "menu");
+        main.frame.add(new GameDisplay(), "gamedisplay");
+        main.frame.lay.show(main.menu.getParent(), "menu");
+    }
+}
--- src/shooter/Main.java
+++ src/shooter/Main.java
@@ -2,15 +2,8 @@ package shooter;
 
 import java.io.IOException;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 import shooter.events.EventHandler;
 import shooter.events.types.GameInitiateEvent;
-import shooter.game.GameDisplay;
 import shooter.menu.shooterMenu;
 
 
@@ -27,54 +20,27 @@ public class Main {
     //Main.class.getResourceAsStream("/resources/strings.txt"))
     public static void main(String[] args) throws IOException {
         instance = new Main();
+        instance.registerListeners();
         instance.initialize();
     }
-    
-    
+
     public void initialize() {
-        registerListeners();
-        //playClip();
-        frame = new GameFrame();
-        menu = new shooterMenu();
-        frame.add(menu, "menu");
-        frame.add(new GameDisplay(), "gamedisplay");
-        frame.lay.show(menu.getParent(), "menu");
         EventHandler.callEvent(new GameInitiateEvent(100));
     }
-    
-    public void playClip() {
-        Thread th = new Thread(() -> {
-            AudioInputStream ais;
-            try {
-                ais = AudioSystem.getAudioInputStream(Main.class.getResourceAsStream("/resources/bomb_x.wav"));
-                Clip test = AudioSystem.getClip();  
 
-                test.open(ais);
-                test.start();
-                Thread.sleep(5000);
-
-                test.close();
-            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } 
-        });
-        th.start();
-    }
-    
     /**
      * Make sure correct listeners are registered first in terms of priority
      */
     public void registerListeners() {
-        EventHandler.registerListener(new TestListener());
+        EventHandler.registerListener(new InitializeListener());
     }
-    
+
     public GameFrame getFrame() {
         return this.frame;
     }
-    
+
     public static Main getInstance() {
         return instance;
     }
-    
+
 }
