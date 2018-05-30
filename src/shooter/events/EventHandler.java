5a740d48f7facf0e65eff2cfde2d3a445990dcb2
Chris <csankey6@gmail.com>
2018-05-29 21:44:45 -0400

    import

--- src/shooter/events/EventHandler.java
+++ src/shooter/events/EventHandler.java
@@ -3,7 +3,6 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class EventHandler {
