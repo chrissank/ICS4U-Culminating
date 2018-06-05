bae229794cfbe4ac963caee1d669e70dc1c0f74c
Mitchell <Mitchell@10.157.9.176>
2018-06-05 12:56:50 -0400

    qwacsacs

--- src/shooter/game/GameDisplay.java
+++ src/shooter/game/GameDisplay.java
@@ -11,16 +11,17 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import shooter.entities.Player;
+import shooter.entities.WeaponType;
+
 public class GameDisplay extends JPanel {
     private static final long serialVersionUID = 1L;
     
-    //REPLACE THESE WITH THE PROPER VARIABLES FROM PLAYER CLASS
     Player player;
-    int playerHealthDemo = 75;
-    int pistolAmmoDemo = 60; 
     
     BufferedImage pistolAmmoImg;
     BufferedImage backgroundImg;
+    BufferedImage playerImage;
     
     public GameDisplay() {
         try {
@@ -57,15 +58,15 @@ public class GameDisplay extends JPanel {
 		int healthBarX = this.getWidth() / 2;
 		int healthBarY = this.getHeight() - 70;
 		g2.draw(new Rectangle2D.Double(healthBarX - 150, healthBarY, 300, 40)); //Health bar border
-		g2.fill(new Rectangle2D.Double(healthBarX - 150, healthBarY, playerHealthDemo * 3, 40)); //Health bar fill
+		g2.fill(new Rectangle2D.Double(healthBarX - 150, healthBarY, player.getHealth() * 3, 40)); //Health bar fill
 		
 		g2.setPaint(Color.BLACK);
 		g2.setFont(new Font("Roboto", Font.PLAIN, 20));
-		g2.drawString(Integer.toString(playerHealthDemo), healthBarX, healthBarY + 60); // Health number below health bar
+		g2.drawString(Integer.toString(player.getHealth()), healthBarX, healthBarY + 60); // Health number below health bar
 		
 		//Pistol ammo
 		g2.drawImage(pistolAmmoImg, 1165, 990, this);
-		g2.drawString(Integer.toString(pistolAmmoDemo), 1240, 1040); 
+		g2.drawString(Integer.toString(player.getAmmo()), 1240, 1040); 
 		
 	
 	}
